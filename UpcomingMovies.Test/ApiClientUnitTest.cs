d605226d3a3f1e2916d63f9827087a81fe762218
Amilton Prado Júnior <amiltonprado@gmail.com>
2019-03-11 21:51:23 -0300

    Ajustes projeto de testes

--- UpcomingMovies.Test/ApiClientUnitTest.cs
+++ UpcomingMovies.Test/ApiClientUnitTest.cs
@@ -3,6 +3,7 @@ using Unity;
 using UpcomingMovies.Application.Services;
 using UpcomingMovies.Domain.Services;
 using UpcomingMovies.Infrastucture.Constants;
+using System.Linq;
 
 namespace UpcomingMovies.Test
 {
@@ -13,17 +14,39 @@ namespace UpcomingMovies.Test
 
         public ApiClientUnitTest()
         {           
-            _container.RegisterType<IUpcomingApiClient, UpcomingApiClient>();            
+            _container.RegisterType<IUpcomingApiClient, UpcomingApiClient>();
+            _container.RegisterType<IMovieDetailApiClient, MovieDetailApiClient>();
         }
 
         [TestMethod]
-        public async void GetUpcomigFirstPage ()
+        public void GetUpcomigFirstPage ()
         {
             var upcomingApiClient = _container.Resolve<IUpcomingApiClient>();
-            var upcoming = await upcomingApiClient.GetUpcomingPage(Localization.PT_BR, 1);
+            var upcoming = upcomingApiClient.GetUpcomingPage(Localization.PT_BR, 1).Result;
 
-            Assert.IsNotNull(upcoming);
+            Assert.IsNotNull(upcoming, "Returned some data");
+            Assert.AreEqual(1, upcoming.page, "Returned first page");
+        }
+
+        [TestMethod]
+        public void GetFirstMovieDetail()
+        {
+            try
+            {
+                var upcomingApiClient = _container.Resolve<IUpcomingApiClient>();
+                var upcoming = upcomingApiClient.GetUpcomingPage(Localization.PT_BR, 1).Result;
+
+                var movieDetailApiClient = _container.Resolve<IMovieDetailApiClient>();
+                var movieDetail = movieDetailApiClient.GetMovieDetail(upcoming.results.FirstOrDefault().id, Localization.PT_BR).Result;
+
+                Assert.IsNotNull(movieDetail, "Returned some detail of first movie");
+            }
+            catch (System.Exception)
+            {
+                Assert.Fail("First movie detail failed");
+            }
 
         }
+
     }
 }
